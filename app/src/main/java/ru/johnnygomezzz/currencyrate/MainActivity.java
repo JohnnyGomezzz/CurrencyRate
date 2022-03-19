9ec70de922f434b7be1769dee76f9c76898e37e3
JohnnyGomezzz <seldesign@mail.ru>
2022-03-20 00:23:18 +0300

    page init

---
+++ app/src/main/java/ru/johnnygomezzz/currencyrate/MainActivity.java
@@ -0,0 +1,150 @@
+package ru.johnnygomezzz.currencyrate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ProgressDialog pd;
+    private final String JSON = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.list);
+
+        new JsonTask().execute(JSON);
+
+    }
+
+    public class Page {
+        @SerializedName("Date")
+        public String date;
+
+        @SerializedName("Valute")
+        public Map<String, Valute> codes;
+    }
+
+    public class Valute {
+        @SerializedName("CharCode")
+        public String charCode;
+
+        @SerializedName("Nominal")
+        public Integer nominal;
+
+        @SerializedName("Name")
+        public String name;
+
+        @SerializedName("Value")
+        public Float value;
+
+        @Override
+        public String toString() {
+            return "\n" + value +
+                    " за " + nominal + " " + name;
+        }
+    }
+
+    public String getCurrencyValue(Page page, String code) {
+        return page.codes.get(code).name + " "
+                + page.codes.get(code).value.toString()
+                + " за " + page.codes.get(code).nominal
+                + " у.е.";
+    }
+
+    public String getAllValues(Page page) {
+        return page.codes.values().toString().replaceAll("(^\\[|\\]$)", "");
+    }
+
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            Gson gson = new Gson();
+            Page page = gson.fromJson(result, Page.class);
+
+            textView.setText(getCurrencyValue(page, "BYN"));
+            textView.setText(getAllValues(page));
+        }
+    }
+}
